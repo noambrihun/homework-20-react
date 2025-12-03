d15636e98dbf5ee897718969dba19dda6c1d0ed6
noambrihun <noambrihun1@gmail.com>
2025-12-03 17:21:55 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
 import { useState } from "react";
-
 import "./App.css";
 
 import original_students from "./data/students.js";
-
 import Header from "./components/Header.jsx";
-import Dashboard from "./components/dashboard.jsx";
+import Dashboard from "./components/Dashboard.jsx";
 import AddStudents from "./components/AddStudents.jsx";
 import Footer from "./components/Footer.jsx";
 
